43d92cd816eb4ea826fb0d9657ca0ba549077f9f
d-burger <dominique.burger.de@gmai.com>
2022-03-18 02:59:46 +0100

    Basic Structure

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+//-------------- IMPORT ----------------------
+import "dotenv/config.js";
+import express from "express";
+import postsRouter from "./routes/postsRouter.js";
+import errorHandler from "./middlewares/errorHandler.js";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//------------ MIDDLEWARES -------------------
+app.use(express.json());
+
+//------------ ROUTE MIDDLEWARES -------------
+app.use("/posts", postsRouter);
+app.use("*", (req, res) => res.send("Blog API"));
+
+//------------ ERROR HANDLING ----------------
+app.use(errorHandler);
+
+//------------ START SERVER ------------------
+app.listen(port, () =>
+  console.log(`Server is running at http://localhost:${port}`)
+);
